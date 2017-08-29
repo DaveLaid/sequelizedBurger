c6141dcfb87859c2763a4d6cf18c535bdc5550f3
David Laidlaw <davidlaidlaw15@gmail.com>
2017-08-29 14:18:58 -0700

    Sending initial creation of folders/files/structure.

---
+++ db/schema.sql
@@ -0,0 +1,10 @@
+
+CREATE DATABASE burgers_db;
+
+USE burgers_db;
+
+SELECT * FROM burgers;
+
+-- DELETE FROM burgers WHERE id = 3;
+
+-- DROP TABLE burgers;
---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name, devoured, date) VALUES ('Behemoth Burger', true, NOW());
+INSERT INTO burgers (burger_name, devoured, date) VALUES ('Double Cheeseburger', true, NOW());
+INSERT INTO burgers (burger_name, devoured, date) VALUES ('4x4 Burger', false, NOW());
+    
+    
